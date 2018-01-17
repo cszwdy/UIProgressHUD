6a7bde481a89ffe36b14bbc8213c613bd56a4e66
Emiaostein <cszwdy@gmail.com>
2018-01-17 11:09:00 +0800

    Initial Commit

---
+++ UIProgressHUD/UIProgressHUD.h
@@ -0,0 +1,19 @@
+//
+//  UIProgressHUD.h
+//  UIProgressHUD
+//
+//  Created by Emiaostein on 16/01/2018.
+//  Copyright © 2018 Emiaostein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIProgressHUD.
+FOUNDATION_EXPORT double UIProgressHUDVersionNumber;
+
+//! Project version string for UIProgressHUD.
+FOUNDATION_EXPORT const unsigned char UIProgressHUDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIProgressHUD/PublicHeader.h>
+
+
